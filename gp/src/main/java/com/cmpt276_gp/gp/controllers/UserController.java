ba14ba618b46663e2409a886533e49f187c1deb0
Sameer-Naqvi <sameerabbasnaqvi189@gmail.com>
2023-07-03 17:27:56 -0700

    added error checking for incomplete form submissions

--- gp/src/main/java/com/cmpt276_gp/gp/controllers/UserController.java
+++ gp/src/main/java/com/cmpt276_gp/gp/controllers/UserController.java
@@ -1,17 +1,13 @@
 package com.cmpt276_gp.gp.controllers;
 
-import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.cmpt276_gp.gp.models.Users;
 import com.cmpt276_gp.gp.models.UserRepository;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -26,19 +22,21 @@ public class UserController {
         String username = loginUser.get("username");
         String password = loginUser.get("password");
         Users user = userRepo.findByUsernameAndPassword(username, password);
-        if (user != null) {
-            String type = user.getUserType();
-            if ("admin".equals(type)) {
-                // landing page for admins
-                return "users/admin/admin";
-            } else if ("teacher".equals(type)) {
+         if (user != null) {
+                String type = user.getUserType();
+                if ("admin".equals(type)) {
+                 // landing page for admins
+                 return "users/admin/admin";
+             } else if ("teacher".equals(type)) {
                 return "users/teacher/teacher";
-            } else {
-                return "users/proctor/proctor";
+             } 
+            else if ("proctor".equals(type)){
+                  return "users/proctor/proctor";
             }
-        }
-        // back to login page with error 
-        return "something for now";
+        } 
+        
+        return "users/login";
+       
     }
     
 
@@ -92,6 +90,9 @@ public class UserController {
         return "users/login";
     }
     
-    
+    @GetMapping("users/error")
+    public String error(){
+        return "users/error";
+    }
 
 }
