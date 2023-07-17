e94b3b2cbd72e4985c51d6432fbe4974804c2517
Sameer-Naqvi <sameerabbasnaqvi189@gmail.com>
2023-07-17 16:39:44 -0700

    Modified admin.html & AdminController.java

--- gp/src/main/java/com/cmpt276_gp/gp/controllers/AdminController.java
+++ gp/src/main/java/com/cmpt276_gp/gp/controllers/AdminController.java
@@ -1,5 +1,6 @@
 package com.cmpt276_gp.gp.controllers;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.cmpt276_gp.gp.models.Admin;
 import com.cmpt276_gp.gp.models.AdminRepository;
+import com.cmpt276_gp.gp.models.Instructor;
+
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -26,6 +29,23 @@ public class AdminController {
     @GetMapping("/admin/exams")
     public String viewExamRequests() {
         return "users/admin/instructorRequests";
+    }
+    @PostMapping("/admin/create")
+    public String createRequest(@RequestParam Map<String, String> admin) {
+
+        String courseName = admin.get("courseName");
+        int duration = Integer.parseInt(admin.get("duration"));
+        int section = Integer.parseInt(admin.get("section"));
+        int noOfRooms = Integer.parseInt(admin.get("noOfRooms"));
+        int noProctors = Integer.parseInt(admin.get("noProctors"));
+        LocalDateTime startTime = LocalDateTime.parse(admin.get("startTime"));
+        int roomsAssigned = Integer.parseInt(admin.get("roomsAssigned"));
+
+
+        // create the instructor exam request
+        Admin newRequest = new Admin(courseName, startTime, section, duration, noOfRooms,  roomsAssigned, noProctors);
+        adminRepo.save(newRequest);
+        return "redirect:/dashboard/admin";
     }
     /*
     @GetMapping(value ="")
