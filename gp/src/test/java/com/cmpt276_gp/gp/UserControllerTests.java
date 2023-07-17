5792480b5105fd87154ffd3bfc143676c4e291db
yangliudev <ylyangliu1@gmail.com>
2023-07-17 11:56:56 -0700

    add tests for UserController.java

--- gp/src/test/java/com/cmpt276_gp/gp/UserControllerTests.java
+++ gp/src/test/java/com/cmpt276_gp/gp/UserControllerTests.java
@@ -12,6 +12,8 @@ import com.cmpt276_gp.gp.models.UserRepository;
 import com.cmpt276_gp.gp.models.Users;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -19,6 +21,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -31,7 +34,7 @@ public class UserControllerTests {
     private UserRepository userRepository;
 
    @Test
-    public void testAdminLogin() throws Exception {
+    public void testValidLogin() throws Exception {
         Users adminUser = new Users("admin", "admin7", "admin", "admin@sfu.ca");
         when(userRepository.findByUsernameAndPassword("admin", "admin7")).thenReturn(adminUser);
 
@@ -42,6 +45,33 @@ public class UserControllerTests {
                 .andExpect(redirectedUrl("/users/admin"));
 
         verify(userRepository).findByUsernameAndPassword("admin", "admin7");
+    }
+
+    @Test
+    public void testInvalidLogin() throws Exception {
+        when(userRepository.findByUsernameAndPassword(anyString(), anyString())).thenReturn(null);
+
+        mockMvc.perform(post("/users/login")
+                .param("username", "invalid")
+                .param("password", "invalid"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("users/login"))
+                .andExpect(model().attributeExists("error"));
+
+        verify(userRepository).findByUsernameAndPassword("invalid", "invalid");
+    }
+
+    @Test
+    public void testAddUser() throws Exception {
+        mockMvc.perform(post("/users/add")
+                .param("username", "unitTest")
+                .param("password", "1")
+                .param("userType", "teacher")
+                .param("email", "newuser@example.com"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/users/login"));
+
+        verify(userRepository).save(any(Users.class));
     }
     
 }
