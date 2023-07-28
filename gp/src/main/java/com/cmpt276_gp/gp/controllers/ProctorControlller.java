413178cc78c7909729154769fc7817bb9c2ebb51
Sameer-Naqvi <sameerabbasnaqvi189@gmail.com>
2023-07-27 20:42:53 -0700

    Proctor Dashboard Implmentation and Creating proctor non avail dates file

--- gp/src/main/java/com/cmpt276_gp/gp/controllers/ProctorControlller.java
+++ gp/src/main/java/com/cmpt276_gp/gp/controllers/ProctorControlller.java
@@ -1,12 +1,20 @@
 package com.cmpt276_gp.gp.controllers;
 
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomCollectionEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +23,9 @@ import com.cmpt276_gp.gp.models.Instructor;
 import com.cmpt276_gp.gp.models.InstructorRepository;
 import com.cmpt276_gp.gp.models.Proctor;
 import com.cmpt276_gp.gp.models.ProctorRepository;
+import com.cmpt276_gp.gp.models.UserRepository;
+import com.cmpt276_gp.gp.models.Users;
+
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -26,16 +37,38 @@ public class ProctorControlller {
     private ProctorRepository procRepo;
     @Autowired
     public InstructorRepository instRepo;
-    @PostMapping("/proctor/non-available-dates")
-    public String nonAvailDates(Model model){
-        return "placeholder";
+    @Autowired
+    public UserRepository userRepo;
+    @GetMapping("/proctor/dashboard")
+    public String showDashboard(Model model){
+        List<Proctor> requests = procRepo.findAll();
+        model.addAttribute("requests", requests);
+        return "users/proctor/proctor";
     }
+    @PostMapping("/proctor/non-available-dates")
+    public String nonAvailDates(@RequestParam("proctorUser") String username, @RequestParam("role") String role, @RequestParam("nonAvailableDates") List<String> nonAvailableDates, Model model){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        List<LocalDate> nonAvailableDateTimes = nonAvailableDates.stream()
+                .map(dateString -> LocalDate.parse(dateString, formatter))
+                .collect(Collectors.toList());
+        Proctor newProc = new Proctor(username, nonAvailableDateTimes, role);
+        procRepo.save(newProc);
+        Users currentUser = userRepo.findByUsername(username);
+        model.addAttribute("user", currentUser);
+        List<Proctor> requests = procRepo.findAll();
+        model.addAttribute("requests", requests);
 
+        return "users/proctor/proctor";
+    }
     @GetMapping("/proctor/pending-exams") 
     public String viewExamRequests(Model model) {
         List<Instructor> teacherTable = instRepo.findAll();
         model.addAttribute("teacherTable", teacherTable);
         return "users/proctor/pendingExams";
+    }
+    @GetMapping("/proctor/my-non-available-dates")
+    public String myNonAvailDates(Model model){
+        return "users/proctor/proctorNADates";
     }
     // create request
     /*
--- gp/src/main/java/com/cmpt276_gp/gp/models/Proctor.java
+++ gp/src/main/java/com/cmpt276_gp/gp/models/Proctor.java
@@ -1,5 +1,5 @@
 package com.cmpt276_gp.gp.models;
-
+import java.time.LocalDate;
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -12,25 +12,44 @@ public class Proctor {
     private int uid;
 
     @ElementCollection
-    private List<LocalDateTime> nonAvailableDates;
-
+    private List<LocalDate> nonAvailableDates;
+    private String username;
+    private String role;
     // Default constructor
     public Proctor() {
 
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     // Parameters Constructor
-    public Proctor(List<LocalDateTime> nonAvailableDates) {
+    public Proctor(String username, List<LocalDate> nonAvailableDates, String role) {
+        this.username = username;
         this.nonAvailableDates = nonAvailableDates;
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 
     // Getters
-    public List<LocalDateTime> getNonAvailableDates() {
+    public List<LocalDate> getNonAvailableDates() {
         return nonAvailableDates;
     }
 
     // Setters
-    public void setNonAvailableDates(List<LocalDateTime> nonAvailableDates) {
+    public void setNonAvailableDates(List<LocalDate> nonAvailableDates) {
         this.nonAvailableDates = nonAvailableDates;
     }
 }
